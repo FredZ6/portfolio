8eaa69ad76692c727dcf6c8ebb329f7b5bf2d052
Fred Zhang <fredzhang026@gmail.com>
2025-02-23 14:41:13 -0600

    email changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,1 +1,9 @@
-<section className="section-padding pt-1 bg-secondary/10" id="home"> 
+const Home = () => {
+  return (
+    <section className="section-padding pt-1 bg-secondary/10" id="home">
+      {/* Your home content here */}
+    </section>
+  )
+}
+
+export default Home 
